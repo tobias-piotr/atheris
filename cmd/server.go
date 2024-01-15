fcbb12091749076dd6bf9ca32bb7cefe04ef37b5
tobias-piotr <piotr.tobiasz.dev@gmail.com>
2024-01-15 11:07:55 +0100

    Add cli for server

---
+++ cmd/server.go
@@ -0,0 +1,51 @@
+/*
+Copyright © 2024 Piotr Tobiasz
+*/
+package cmd
+
+import (
+	"log/slog"
+
+	"atheris/libs"
+	"atheris/requests"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+)
+
+var (
+	dbFilename string
+	port       string
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Starts server",
+	Long:  "Starts atheris server, that will catch all requests and store them in database",
+	Run: func(_ *cobra.Command, _ []string) {
+		slog.Info("Starting atheris server")
+
+		db := libs.GetDBConnection(dbFilename)
+		libs.MigrateDB(db)
+
+		handler := requests.NewAPIHandler(db)
+
+		e := echo.New()
+
+		e.Any("/*", handler.HandleRequest)
+
+		// TODO: Replace with slog
+		e.Logger.Fatal(e.Start(":" + port))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// SQLite database file
+	serverCmd.Flags().StringVarP(&dbFilename, "db", "d", "database.sqlite", "SQLite database file")
+
+	// Server port
+	serverCmd.Flags().StringVarP(&port, "port", "p", "8888", "Port to listen on")
+}
