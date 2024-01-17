71fc7c5a26645daee367a541fc3cf0e49f52905b
Piotr Tobiasz <49806746+tobias-piotr@users.noreply.github.com>
2024-01-17 09:32:42 +0100

    Add basic requests list (#5)

--- requests/tui/model.go
+++ requests/tui/model.go
@@ -1,9 +1,16 @@
 package tui
 
 import (
+	"fmt"
+	"os"
+	"time"
+
+	"atheris/requests"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/jmoiron/sqlx"
 )
 
 var (
@@ -13,54 +20,70 @@ var (
 				Render
 )
 
+// Item represents a request on the list
 type item struct {
-	title string
-	desc  string
+	id        string
+	createdAt time.Time
+	path      string
+	name      string
 }
 
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.title }
+func (i item) Title() string {
+	if i.name == "" {
+		return i.id
+	}
+	return i.name
+}
+
+func (i item) Description() string {
+	// TODO: Add method
+	return fmt.Sprintf("GET %s at %s", i.path, i.createdAt)
+}
+
+func (i item) FilterValue() string {
+	if i.name == "" {
+		return i.id
+	}
+	return i.name
+}
+
+func ItemsFromRequests(rqs []requests.Request) []list.Item {
+	items := make([]list.Item, len(rqs))
+	for i, rq := range rqs {
+		items[i] = item{
+			id:        rq.ID.String(),
+			createdAt: rq.CreatedAt,
+			path:      rq.Path,
+			name:      "",
+		}
+	}
+	return items
+}
 
 type model struct {
 	list         list.Model
 	delegateKeys *delegateKeyMap
+	db           *sqlx.DB
 }
 
-// TODO: Replace with requests
-func NewModel(items []list.Item) model {
-	defaultItems := []list.Item{
-		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{title: "Nutella", desc: "It's good on toast"},
-		item{title: "Bitter melon", desc: "It cools you down"},
-		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{title: "Eight hours of sleep", desc: "I had this once"},
-		item{title: "Cats", desc: "Usually"},
-		item{title: "Plantasia, the album", desc: "My plants love it too"},
-		item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		item{title: "Linux", desc: "Pretty much the best OS"},
-		item{title: "Business school", desc: "Just kidding"},
-		item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		item{title: "Shampoo", desc: "Nothing like clean hair"},
-		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
-		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
-		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
-		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+func NewModel(db *sqlx.DB) model {
+	rqs, err := requests.GetRequests(db)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting requests: %v\n", err)
+		os.Exit(1)
 	}
+	items := ItemsFromRequests(rqs)
+
 	delKeys := newDelegateKeyMap()
 	delegate := newItemDelegate(delKeys)
-	itemList := list.New(defaultItems, delegate, 0, 0)
+
+	itemList := list.New(items, delegate, 0, 0)
 	itemList.Title = "Requests"
+
 	return model{
 		list:         itemList,
 		delegateKeys: delKeys,
+		db:           db,
 	}
 }
 
